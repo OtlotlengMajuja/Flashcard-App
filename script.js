7377e6d076a7268710257e866c704d51c80c6ea9
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-23 15:42:04 +0200

    Added DOM element selectors, initialized variables, and event listener for add question modal

---
+++ script.js
@@ -0,0 +1,22 @@
+// Selecting DOM elements
+const container = document.querySelector(".container");
+const addQuestionModal = document.getElementById("add-card-modal");
+const cardButton = document.getElementById("save-btn");
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const errorMessage = document.getElementById("error");
+const addQuestion = document.getElementById("add-card");
+const closeBtn = document.getElementById("close-btn");
+
+// Initializing variables
+let editBool = false;
+let originalId = null;
+let flashcards = JSON.parse(localStorage.getItem('flashcards')) || [];
+
+addQuestion.addEventListener('click', () => {
+    // Show the add question modal and hide the container
+    container.classList.add('hide');
+    question.value = "";
+    answer.value = "";
+    addQuestionModal.classList.remove('hide');
+});
